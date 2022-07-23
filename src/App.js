3b3e03c3f2e8932610b3987c44b7eec072965af5
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2022-07-23 16:03:42 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+    
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
